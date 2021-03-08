3537bc9f16ac238af6135cb222e0ec8bfdf56ddf
jensbrak <jensbrak@gmail.com>
2021-03-08 21:14:01 +0100

    Resolve conflicting _ViewImports.cshtml

--- src/Areas/Manager/Pages/SpamDetectorConfigEdit.cshtml.cs
+++ src/Areas/Manager/Pages/SpamDetectorConfigEdit.cshtml.cs
@@ -1,12 +1,6 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Zon3.SpamDetector.Areas.Manager.Pages
+namespace Zon3.SpamDetector.Areas.Manager.Pages.SpamDetectorConfigEdit
 {
     class SpamDetectorConfigEdit : PageModel
     {
