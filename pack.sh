968f8b30bd175ef1d443acdc1e006605e36e19bb
jensbrak <52660827+jensbrak@users.noreply.github.com>
2021-11-29 15:36:01 +0100

    Fix

--- pack.sh
+++ pack.sh
@@ -6,4 +6,5 @@ dotnet clean
 dotnet build -c Release
 
 # Create NuGet packages
-dotnet pack Zon3.SpamDetector/Zon3.SpamDetector --no-build -c Release -o ./artifacts
+dotnet pack Zon3.SpamDetector/Zon3.SpamDetector.csproj --no-build -c Release -o ./artifacts
+dotnet pack Zon3.SpamDetector.Localization/Zon3.SpamDetector.Localization.csproj --no-build -c Release -o ./artifacts
